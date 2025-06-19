e2f01abee6194b3540e80e19ceae0e718989e718
pydev2007 <80983612+pydev2007@users.noreply.github.com>
2025-06-19 14:34:51 -0600

    Delete src/pages/api/chat/users directory

--- src/pages/api/chat/users/index.js
+++
@@ -1,7 +0,0 @@
-import users from 'data/chat.json';
-import cors from 'utils/cors';
-
-export default async function handler(req, res) {
-  await cors(req, res);
-  return res.status(200).json({ users });
-}
